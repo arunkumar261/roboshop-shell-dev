227a766abb8cac3822234bc90ea59b2ac4e2cff0
arunkumar261 <arunkumarreddy261@gmail.com>
2025-05-31 20:36:17 +0530

    resolved issue in web script

--- web.sh
+++ web.sh
@@ -10,15 +10,15 @@ N="\e[0m"
 TIMESTAMP=$(date +%F-%H-%M-%S)
 LOG_FILE="/tmp/$0-$TIMESTAMP.log"
 
-echo "script stareted executing at $TIMESTAMP" &>> $LOGFILE
+echo "script stareted executing at $TIMESTAMP" &>> $LOG_FILE
 
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        else -e "$R ..... FAILED $N "   
+        echo -e "$R ..... FAILED $N"   
         exit 1
     else
-        else -e "$G ..... SUCCESS $N "
+        echo -e "$G ..... SUCCESS $N"
     fi
 }
 
