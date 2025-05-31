5288d10964f91da773ba47c29da00dc0622756f3
arunkumar261 <arunkumarreddy261@gmail.com>
2025-05-31 20:34:53 +0530

    resolved issue in mongodb script

--- mongodb.sh
+++ mongodb.sh
@@ -11,7 +11,7 @@ N="\e[0m"
 TIMESTAMP=$(date +%F-%H-%M-%S)
 LOG_FILE="/tmp/$0-$TIMESTAMP.log"
 
-echo "script stareted executing at $TIMESTAMP" &>> $LOGFILE
+echo "script stareted executing at $TIMESTAMP" &>> $LOG_FILE
 
 VALIDATE(){
     if [ $1 -ne 0 ]
