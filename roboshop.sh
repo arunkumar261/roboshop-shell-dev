28bed36b20cddabd074d3ad02de99094cb47a824
arunkumar261 <arunkumarreddy261@gmail.com>
2025-06-01 07:05:21 +0530

    Script for creating ec2 instances

--- roboshop.sh
+++ roboshop.sh
@@ -2,7 +2,7 @@
 
 AMI_ID=ami-0b4f379183e5706b9
 SG_ID=sg-0dccc737241e3f233
-INSTANCES=("mongdb","redis","mysql","rabbitmq","catalogue","user","cart","shipping","payment","dispatch","web" )
+INSTANCES=("mongdb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web" )
 
 
 for i in "${INSTANCES[@]}"
