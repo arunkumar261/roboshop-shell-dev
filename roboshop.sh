a5e6f289af7edfb6e031933031dd943be820cf08
arunkumar261 <arunkumarreddy261@gmail.com>
2025-06-01 07:47:19 +0530

    Script for creating ec2 instances

--- roboshop.sh
+++ roboshop.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-AMI=ami-0b4f379183e5706b9 #this keeps on changing
-SG_ID=sg-0dccc737241e3f233 #replace with your SG ID
-INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
-# ZONE_ID=Z00836682TJ775V3EAIOJ # replace your zone ID
-# DOMAIN_NAME="arundev.store"
+AMI_ID=ami-0b4f379183e5706b9
+SG_ID=sg-0dccc737241e3f233
+INSTANCES=("mongdb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
+
 
 for i in "${INSTANCES[@]}"
 do
-    echo "Instance is : $i"
     if [ $i == "mongodb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
     then
         INSTANCE_TYPE="t3.small"
@@ -16,5 +14,7 @@ do
         INSTANCE_TYPE="t2.micro"
     fi
 
-    aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --security-group-ids sg-0dccc737241e3f233 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]"
+    IP_ADDRESS=(aws ec2 run-instances --image-id $AMI_ID --instance-type $INSTANCE_TYPE --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
+
+    echo "$i : $IP_ADDRESS"
 done
