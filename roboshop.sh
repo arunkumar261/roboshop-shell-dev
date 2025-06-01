c70e56cde211d5c54da554838f4e7a6d777ffe08
arunkumar261 <arunkumarreddy261@gmail.com>
2025-06-01 07:55:58 +0530

    Script for creating ec2 instances

--- roboshop.sh
+++ roboshop.sh
@@ -29,7 +29,7 @@ do
         "Action"              : "CREATE"
         ,"ResourceRecordSet"  : {
             "Name"              : "'$i'.'$DOMAIN_NAME'"
-            ,"Type"             : "CNAAME"
+            ,"Type"             : "A"
             ,"TTL"              : 1
             ,"ResourceRecords"  : [{
                 "Value"         : "'$IP_ADDRESS'"
