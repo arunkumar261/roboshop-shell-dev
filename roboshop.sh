0822b12cc75430b9bc93f60a4198b6a8548a6a3d
arunkumar261 <arunkumarreddy261@gmail.com>
2025-06-01 07:01:18 +0530

    Script for creating ec2 instances

--- roboshop.sh
+++ roboshop.sh
@@ -7,11 +7,11 @@ INSTANCES=("mongdb","redis","mysql","rabbitmq","catalogue","user","cart","shippi
 
 for i in "${INSTANCES[@]}"
 do
-if [ $i == "mongodb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
-then
-    INSTANCE_TYPE="t3.small"
-else
-    INSTANCE_TYPE="t2.micro"
-fi
+    if [ $i == "mongodb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
+    then
+        INSTANCE_TYPE="t3.small"
+    else
+        INSTANCE_TYPE="t2.micro"
+    fi
     aws ec2 run-instances --image-id $AMI_ID --instance-type $INSTANCE_TYPE --security-group-ids $SG_ID
 done
